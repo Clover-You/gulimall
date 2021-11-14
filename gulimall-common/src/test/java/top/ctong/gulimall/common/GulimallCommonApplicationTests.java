cb373c60c008d7390f5b7034bd8803fd110060f2
Clover You <2621869236@qq.com>
2021-11-14 16:58:57 +0800

    新建公共模块

---
+++ gulimall-common/src/test/java/top/ctong/gulimall/common/GulimallCommonApplicationTests.java
@@ -0,0 +1,13 @@
+package top.ctong.gulimall.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCommonApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
