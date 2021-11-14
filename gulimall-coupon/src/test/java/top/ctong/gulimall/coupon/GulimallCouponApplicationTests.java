9a8f9d186b26c9de12b61633e7f297ddb679ab59
Clover You <2621869236@qq.com>
2021-11-14 15:35:00 +0800

    项目初始结构创建

---
+++ gulimall-coupon/src/test/java/top/ctong/gulimall/coupon/GulimallCouponApplicationTests.java
@@ -0,0 +1,13 @@
+package top.ctong.gulimall.coupon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCouponApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
