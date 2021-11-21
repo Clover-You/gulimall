6ee091aaa4d35dfa5d44a16d2f875ad88759323f
Clover You <2621869236@qq.com>
2021-11-21 16:53:40 +0800

    添加网关模块

---
+++ gulimall-gateway/src/main/java/top/ctong/gulimall/gateway/GulimallGatewayApplication.java
@@ -0,0 +1,33 @@
+package top.ctong.gulimall.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+/**
+ * █████▒█      ██  ▄████▄   ██ ▄█▀     ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒      ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░      ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄      ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄     ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒     ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ * Copyright 2021 Clover You.
+ * <p>
+ * 网关
+ * </p>
+ *
+ * @author Clover You
+ * @create 2021/11/21 15:34
+ */
+@EnableDiscoveryClient
+@SpringBootApplication
+public class GulimallGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallGatewayApplication.class, args);
+    }
+
+}
