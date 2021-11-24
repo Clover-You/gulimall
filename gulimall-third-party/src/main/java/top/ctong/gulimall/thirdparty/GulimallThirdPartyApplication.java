124c4a78b47eca8967977717f5a6040b13622ef3
Clover You <2621869236@qq.com>
2021-11-24 09:31:39 +0800

    oss配置

--- gulimall-third-party/src/main/java/top/ctong/gulimall/thirdparty/GulimallThirdPartyApplication.java
+++ gulimall-third-party/src/main/java/top/ctong/gulimall/thirdparty/GulimallThirdPartyApplication.java
@@ -2,7 +2,27 @@ package top.ctong.gulimall.thirdparty;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+/**
+ * █████▒█      ██  ▄████▄   ██ ▄█▀     ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒      ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░      ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄      ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄     ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒     ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ * Copyright 2021 Clover You.
+ * <p>
+ * 第三方服务
+ * </p>
+ *
+ * @author Clover You
+ * @create 2021/11/24 09:07
+ */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class GulimallThirdPartyApplication {
 
