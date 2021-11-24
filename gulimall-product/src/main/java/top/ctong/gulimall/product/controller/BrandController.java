6d05a2ec39a1a8e2d2f9326cf5fdc4d77334d662
Clover You <2621869236@qq.com>
2021-11-24 08:37:35 +0800

    合并

--- gulimall-product/src/main/java/top/ctong/gulimall/product/controller/BrandController.java
+++ gulimall-product/src/main/java/top/ctong/gulimall/product/controller/BrandController.java
@@ -31,7 +31,6 @@ import top.ctong.gulimall.common.utils.R;
  * <p>
  * 品牌
  * </p>
- *
  * @author Clover You
  * @email 2621869236@qq.com
  * @create 2021-11-15 09:51:26
@@ -47,7 +46,7 @@ public class BrandController {
      */
     @RequestMapping("/list")
     //@RequiresPermissions("product:brand:list")
-    public R list(@RequestParam Map<String, Object> params){
+    public R list(@RequestParam Map<String, Object> params) {
         PageUtils page = brandService.queryPage(params);
 
         return R.ok().put("page", page);
@@ -59,8 +58,8 @@ public class BrandController {
      */
     @RequestMapping("/info/{brandId}")
     //@RequiresPermissions("product:brand:info")
-    public R info(@PathVariable("brandId") Long brandId){
-		BrandEntity brand = brandService.getById(brandId);
+    public R info(@PathVariable("brandId") Long brandId) {
+        BrandEntity brand = brandService.getById(brandId);
 
         return R.ok().put("brand", brand);
     }
@@ -70,19 +69,22 @@ public class BrandController {
      */
     @RequestMapping("/save")
     //@RequiresPermissions("product:brand:save")
-    public R save(@RequestBody BrandEntity brand){
-		brandService.save(brand);
+    public R save(@RequestBody BrandEntity brand) {
+        brandService.save(brand);
 
         return R.ok();
     }
 
     /**
-     * 修改
+     * 修改品牌信息
+     * @param brand 品牌信息
+     * @return R
+     * @author Clover You
+     * @date 2021/11/23 16:25
      */
     @RequestMapping("/update")
-    //@RequiresPermissions("product:brand:update")
-    public R update(@RequestBody BrandEntity brand){
-		brandService.updateById(brand);
+    public R update(@RequestBody BrandEntity brand) {
+        brandService.updateById(brand);
 
         return R.ok();
     }
@@ -92,8 +94,8 @@ public class BrandController {
      */
     @RequestMapping("/delete")
     //@RequiresPermissions("product:brand:delete")
-    public R delete(@RequestBody Long[] brandIds){
-		brandService.removeByIds(Arrays.asList(brandIds));
+    public R delete(@RequestBody Long[] brandIds) {
+        brandService.removeByIds(Arrays.asList(brandIds));
 
         return R.ok();
     }
