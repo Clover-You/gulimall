67f1285338f375b51cf35e5bb105ddb54cd87bde
Clover You <2621869236@qq.com>
2021-12-14 11:02:50 +0800

    全局分页插件插件

---
+++ gulimall-common/src/main/java/top/ctong/gulimall/common/config/MybatisPlusConfig.java
@@ -0,0 +1,55 @@
+package top.ctong.gulimall.common.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.autoconfigure.MybatisPlusAutoConfiguration;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingClass;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+/**
+ * █████▒█      ██  ▄████▄   ██ ▄█▀     ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒      ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░      ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄      ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄     ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒     ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ * Copyright 2021 Clover You.
+ * <p>
+ * MyBatis配置文件
+ * {EnableTransactionManagement}开启事务
+ * </p>
+ * @author Clover You
+ * @create 2021-11-27 09:28
+ */
+@Configuration
+@ConditionalOnClass(MybatisPlusAutoConfiguration.class)
+@EnableTransactionManagement
+@MapperScan("top.ctong.gulimall.**.dao")
+public class MybatisPlusConfig {
+
+    @Configuration
+    @ConditionalOnClass(MybatisPlusInterceptor.class)
+    static class MybatisPlusInterceptorAutoConfiguration {
+        /**
+         * 分页插件
+         * @return MybatisPlusInterceptor
+         * @author Clover You
+         * @date 2021/11/27 09:39
+         */
+        @ConditionalOnMissingBean(MybatisPlusInterceptor.class)
+        public MybatisPlusInterceptor mybatisPlusInterceptor() {
+            MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+            interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+            return interceptor;
+        }
+    }
+}
