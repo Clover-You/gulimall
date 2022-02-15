1fd931374f033fa0ab775ea7f163f022fd91f579
Clover You <50982977+Clover-You@users.noreply.github.com>
2022-02-16 00:58:32 +0800

    检索服务集成SpringSession

--- gulimall-search/src/main/java/top/ctong/gulimall/search/GulimallSearchApplication.java
+++ gulimall-search/src/main/java/top/ctong/gulimall/search/GulimallSearchApplication.java
@@ -3,13 +3,16 @@ package top.ctong.gulimall.search;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
 import top.ctong.gulimall.common.feign.ProductFeignService;
 //import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 //import top.ctong.gulimall.common.config.MybatisPlusConfig;
 
-//@EnableDiscoveryClient
+@EnableRedisHttpSession
+@EnableDiscoveryClient
 @EnableFeignClients(clients = {ProductFeignService.class})
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class GulimallSearchApplication {
---
+++ gulimall-search/src/main/java/top/ctong/gulimall/search/config/SpringSessionConfig.java
@@ -0,0 +1,44 @@
+package top.ctong.gulimall.search.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializer;
+import org.springframework.session.web.http.CookieSerializer;
+import org.springframework.session.web.http.DefaultCookieSerializer;
+import top.ctong.gulimall.common.constant.CommonConstant;
+
+/**
+ * █████▒█      ██  ▄████▄   ██ ▄█▀     ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒      ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░      ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄      ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄     ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒     ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ * Copyright 2022 Clover You.
+ * <p>
+ * springsession 配置
+ * </p>
+ * @author Clover You
+ * @email 2621869236@qq.com
+ * @create 2022-02-16 12:40 上午
+ */
+@Configuration
+public class SpringSessionConfig {
+
+    @Bean
+    public CookieSerializer cookieSerializer() {
+        DefaultCookieSerializer serializer = new DefaultCookieSerializer();
+        serializer.setDomainName(CommonConstant.SESSION_COOKIE_DOMAIN_NAME);
+        serializer.setCookieName(CommonConstant.SESSION_COOKIE_NAME);
+        return serializer;
+    }
+
+    @Bean
+    public RedisSerializer<Object> springSessionDefaultRedisSerializer() {
+        return new GenericJackson2JsonRedisSerializer();
+    }
+}
