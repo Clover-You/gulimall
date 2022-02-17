a5f5dee7775717020eddfef0e028814fd9f3b0a1
Clover You <50982977+Clover-You@users.noreply.github.com>
2022-02-17 14:24:13 +0800

    购物车

---
+++ gulimall-cart/src/main/java/top/ctong/gulimall/cart/GulimallCartApplication.java
@@ -0,0 +1,13 @@
+package top.ctong.gulimall.cart;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallCartApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GulimallCartApplication.class, args);
+	}
+
+}
---
+++ gulimall-cart/src/test/java/top/ctong/gulimall/cart/GulimallCartApplicationTests.java
@@ -0,0 +1,13 @@
+package top.ctong.gulimall.cart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
