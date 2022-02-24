200151c9f97ef79c7e535c6bc1cc47c30e24ff4f
Clover You <50982977+Clover-You@users.noreply.github.com>
2022-02-24 20:33:57 +0800

    修复页面错误

---
+++ gulimall-order/src/main/java/top/ctong/gulimall/order/web/PageController.java
@@ -0,0 +1,36 @@
+package top.ctong.gulimall.order.web;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+/**
+ * █████▒█      ██  ▄████▄   ██ ▄█▀     ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒      ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░      ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄      ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄     ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒     ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ * Copyright 2022 Clover You.
+ * <p>
+ * 页面映射前端控制器
+ * </p>
+ * @author Clover You
+ * @email 2621869236@qq.com
+ * @create 2022-02-24 8:17 下午
+ */
+@Controller
+@Slf4j
+public class PageController {
+
+    @GetMapping("/{page}.html")
+    public String listPage(@PathVariable("page") String pageName) {
+        log.info("pageName: ===>> {}", pageName);
+        return pageName;
+    }
+
+}
