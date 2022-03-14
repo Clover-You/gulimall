bd8468c635159b9f4b12de75fbfc008acf85f18d
Clover You <cloveryou02@163.com>
2022-03-14 15:22:59 +0800

    秒杀服务基础环境配置

--- gulimall-seckill/src/main/java/top/ctong/gulimall/seckill/GulimallSeckillApplication.java
+++ gulimall-seckill/src/main/java/top/ctong/gulimall/seckill/GulimallSeckillApplication.java
@@ -2,8 +2,11 @@ package top.ctong.gulimall.seckill;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-@SpringBootApplication
+@EnableDiscoveryClient
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class GulimallSeckillApplication {
 
     public static void main(String[] args) {
