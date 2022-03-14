a97ffe654de35459ba2482158656f172450b05a2
Clover You <cloveryou02@163.com>
2022-03-14 14:44:42 +0800

    添加秒杀模块

---
+++ gulimall-seckill/src/main/java/top/ctong/gulimall/seckill/GulimallSeckillApplication.java
@@ -0,0 +1,13 @@
+package top.ctong.gulimall.seckill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GulimallSeckillApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GulimallSeckillApplication.class, args);
+    }
+
+}
---
+++ gulimall-seckill/src/test/java/top/ctong/gulimall/seckill/GulimallSeckillApplicationTests.java
@@ -0,0 +1,13 @@
+package top.ctong.gulimall.seckill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GulimallSeckillApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
