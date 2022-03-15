d7f76d60da3df50ee5487c2aa5bbb7f242f4f3fe
Clover You <cloveryou02@163.com>
2022-03-15 08:11:28 +0800

    添加Redisson

---
+++ gulimall-seckill/src/main/java/top/ctong/gulimall/seckill/config/RedissonConfig.java
@@ -0,0 +1,44 @@
+package top.ctong.gulimall.seckill.config;
+
+import org.redisson.Redisson;
+import org.redisson.api.RedissonClient;
+import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * █████▒█      ██  ▄████▄   ██ ▄█▀     ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒      ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░      ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄      ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄     ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒     ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ * Copyright 2022 Clover You.
+ * <p>
+ * redisson 配置
+ * </p>
+ * @author Clover You
+ * @email cloveryou02@163.com
+ * @create 2022-03-15 8:04 上午
+ */
+@Configuration
+public class RedissonConfig {
+
+    @Value("${spring.redis.host}")
+    private final String REDIS_HOST = "localhost";
+
+    @Value("${spring.redis.port}")
+    private final Integer REDIS_PORT = 6379;
+
+    @Bean(destroyMethod = "shutdown")
+    public RedissonClient redissonClient() {
+        Config config = new Config();
+        config.useSingleServer().setAddress("redis://" + REDIS_HOST + ":" + REDIS_PORT);
+        return Redisson.create(config);
+    }
+
+}
