2dba9130c82b75787bd85d185717f04af86bbf9a
Clover You <cloveryou02@163.com>
2022-03-20 09:41:51 +0800

    全服务集成Sentinel

--- gulimall-seckill/src/main/java/top/ctong/gulimall/seckill/config/SentinelConfig.java
+++ gulimall-seckill/src/main/java/top/ctong/gulimall/seckill/config/SentinelConfig.java
@@ -1,7 +1,5 @@
 package top.ctong.gulimall.seckill.config;
 
-import com.alibaba.csp.sentinel.adapter.spring.webflux.callback.BlockRequestHandler;
-import com.alibaba.csp.sentinel.adapter.spring.webflux.callback.WebFluxCallbackManager;
 import com.alibaba.csp.sentinel.adapter.spring.webmvc.callback.BlockExceptionHandler;
 import com.alibaba.csp.sentinel.slots.block.BlockException;
 import com.alibaba.fastjson.JSON;
@@ -9,10 +7,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
-import org.springframework.web.reactive.function.BodyInserters;
-import org.springframework.web.reactive.function.server.ServerResponse;
-import org.springframework.web.server.ServerWebExchange;
-import reactor.core.publisher.Mono;
 import top.ctong.gulimall.common.exception.BizCodeEnum;
 import top.ctong.gulimall.common.utils.R;
 
