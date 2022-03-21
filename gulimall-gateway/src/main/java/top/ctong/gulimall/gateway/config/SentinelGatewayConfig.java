8dc7e37c8a650a4722b1b97ae1ef0f89b2b10365
Clover You <cloveryou02@163.com>
2022-03-21 09:10:24 +0800

    配置网关流控降级处理数据

---
+++ gulimall-gateway/src/main/java/top/ctong/gulimall/gateway/config/SentinelGatewayConfig.java
@@ -0,0 +1,43 @@
+package top.ctong.gulimall.gateway.config;
+
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.BlockRequestHandler;
+import com.alibaba.csp.sentinel.adapter.gateway.sc.callback.GatewayCallbackManager;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+import top.ctong.gulimall.common.exception.BizCodeEnum;
+import top.ctong.gulimall.common.utils.R;
+
+/**
+ * █████▒█      ██  ▄████▄   ██ ▄█▀     ██████╗ ██╗   ██╗ ██████╗
+ * ▓██   ▒ ██  ▓██▒▒██▀ ▀█   ██▄█▒      ██╔══██╗██║   ██║██╔════╝
+ * ▒████ ░▓██  ▒██░▒▓█    ▄ ▓███▄░      ██████╔╝██║   ██║██║  ███╗
+ * ░▓█▒  ░▓▓█  ░██░▒▓▓▄ ▄██▒▓██ █▄      ██╔══██╗██║   ██║██║   ██║
+ * ░▒█░   ▒▒█████▓ ▒ ▓███▀ ░▒██▒ █▄     ██████╔╝╚██████╔╝╚██████╔╝
+ * ▒ ░   ░▒▓▒ ▒ ▒ ░ ░▒ ▒  ░▒ ▒▒ ▓▒     ╚═════╝  ╚═════╝  ╚═════╝
+ * ░     ░░▒░ ░ ░   ░  ▒   ░ ░▒ ▒░
+ * ░ ░    ░░░ ░ ░ ░        ░ ░░ ░
+ * ░     ░ ░      ░  ░
+ * Copyright 2022 Clover You.
+ * <p>
+ * 网关限流配置
+ * </p>
+ * @author Clover You
+ * @email cloveryou02@163.com
+ * @create 2022-03-21 8:43 上午
+ */
+@Configuration
+public class SentinelGatewayConfig {
+
+    public SentinelGatewayConfig() {
+        GatewayCallbackManager.setBlockHandler(new BlockRequestHandler() {
+            @Override
+            public Mono<ServerResponse> handleRequest(ServerWebExchange serverWebExchange, Throwable throwable) {
+                R error = R.error(BizCodeEnum.TO_MANY_REQUEST);
+                return ServerResponse.ok().body(Mono.just(error), R.class);
+            }
+        });
+    }
+
+}
